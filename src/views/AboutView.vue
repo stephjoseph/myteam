37a7671a96e9c765057b415a8cba2e04c2f96323
Stephen Joseph Muñez <sjtmunez@gmail.com>
2023-02-26 17:44:43 +0800

    feat: about CTA

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,10 +2,12 @@
 import AboutHero from '../components/AboutHero.vue';
 import AboutDirectors from '../components/AboutDirectors.vue';
 import AboutClients from '../components/AboutClients.vue';
+import CallToAction from '../components/CallToAction.vue';
 </script>
 
 <template>
   <AboutHero />
   <AboutDirectors />
   <AboutClients />
+  <CallToAction />
 </template>
