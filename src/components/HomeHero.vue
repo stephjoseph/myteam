6f2fc465fae30adb331e4f1ef2868ed1805a66c1
Stephen Joseph Muñez <sjtmunez@gmail.com>
2023-02-27 17:45:49 +0800

    feat: home hero tablet

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -4,15 +4,17 @@ import BgPatternHome2 from './icons/BgPatternHome2.vue';
 
 <template>
   <section
-    class="relative flex w-[87.2%] flex-col items-center gap-[1.125rem] pb-[12.313rem] text-center"
+    class="relative flex w-[87.2%] flex-col items-center gap-[1.125rem] pb-[12.313rem] text-center md:w-[88.93%] md:gap-6 md:pb-64"
   >
     <div class="pointer-events-none absolute bottom-0 z-0">
       <BgPatternHome2 />
     </div>
-    <h1 class="z-10 text-[2.5rem] font-bold leading-[2.5rem]">
+    <h1
+      class="z-10 text-[2.5rem] font-bold leading-[2.5rem] tracking-normal md:text-[4rem] md:leading-[3.5rem]"
+    >
       Find the<br />best <span class="text-light-coral">talent</span>
     </h1>
-    <p class="font-body-2 z-10">
+    <p class="font-body-2 z-10 md:w-[66.91%]">
       Finding the right people and building high performing teams can be hard. Most companies aren’t
       tapping into the abundance of global talent. We’re about to change that.
     </p>
