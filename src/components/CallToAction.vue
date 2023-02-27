94f169c86303481a5445df99e342067f199b927b
Stephen Joseph Muñez <sjtmunez@gmail.com>
2023-02-27 17:28:23 +0800

    feat: cta tablet

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -4,12 +4,14 @@ import BgPatternHome6About5 from './icons/BgPatternHome6About5.vue';
 
 <template>
   <section
-    class="relative flex w-full flex-col items-center overflow-y-hidden bg-light-coral py-[5.188rem]"
+    class="relative flex w-full flex-col items-center overflow-y-hidden bg-light-coral py-[5.188rem] md:py-[4.75rem]"
   >
     <div class="pointer-events-none absolute bottom-0 left-0 translate-y-[22px]">
       <BgPatternHome6About5 />
     </div>
-    <div class="z-10 flex w-[87.2%] flex-col items-center gap-6">
+    <div
+      class="z-10 flex w-[87.2%] flex-col items-center gap-6 md:w-[74.60%] md:flex-row md:justify-between"
+    >
       <h2
         class="text-center text-[2rem] font-bold leading-8 tracking-normal text-sacramento-state-green"
       >
