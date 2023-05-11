b6e14770f8eff6070c184c4749bb2c1b58bf535c
Stephen Joseph Muñez <sjtmunez@gmail.com>
2023-05-11 10:46:32 +0800

    feat: footer desktop

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,6 @@
 <script setup>
 import LogoIconMobile from './icons/LogoIconMobile.vue';
+import LogoIcon from './icons/LogoIcon.vue';
 import IconFacebook from './icons/IconFacebook.vue';
 import IconTwitter from './icons/IconTwitter.vue';
 import IconPinterest from './icons/IconPinterest.vue';
@@ -7,33 +8,46 @@ import IconPinterest from './icons/IconPinterest.vue';
 
 <template>
   <footer class="flex w-full flex-col items-center bg-dark-green py-16 md:py-14">
-    <div class="flex w-[87.2%] flex-col items-center gap-10 md:w-[89.71%] md:gap-8">
-      <div class="flex w-full flex-col items-center gap-6 md:flex-row md:justify-between">
+    <div
+      class="flex w-[87.2%] flex-col items-center gap-10 md:w-[89.71%] md:gap-8 xl:w-[77.08%] xl:flex-row xl:items-start xl:justify-between"
+    >
+      <div
+        class="flex w-full flex-col items-center gap-6 md:flex-row md:justify-between xl:justify-start xl:gap-[7.813rem]"
+      >
         <div class="flex w-full flex-col items-center md:w-auto">
-          <div class="flex w-full flex-col items-center gap-6 md:items-start md:gap-14">
-            <a href="/">
+          <div class="flex w-full flex-col items-center gap-6 md:items-start md:gap-14 xl:gap-6">
+            <a href="/" class="block xl:hidden">
               <LogoIconMobile />
+            </a>
+            <a href="/" class="hidden xl:block">
+              <LogoIcon />
             </a>
             <nav class="flex w-full flex-col items-center md:items-start">
               <ul class="flex items-center gap-6">
                 <li>
-                  <router-link class="font-body-2 text-white" to="/">home</router-link>
+                  <router-link class="font-body-2 text-white xl:text-lg" to="/">home</router-link>
                 </li>
                 <li>
-                  <router-link class="font-body-2 text-white" to="/about">about</router-link>
+                  <router-link class="font-body-2 text-white xl:text-lg" to="/about"
+                    >about</router-link
+                  >
                 </li>
               </ul>
             </nav>
           </div>
         </div>
-        <div class="font-body-2 w-full text-center text-white/60 md:w-auto md:text-right">
+        <div
+          class="font-body-2 w-full text-center text-white/60 md:w-auto md:text-right xl:text-left"
+        >
           987 Hillcrest Lane<br />
           Irvine, CA<br />
           California 92714<br />
           Call Us : 949-833-7432
         </div>
       </div>
-      <div class="flex w-full flex-col items-center gap-4 md:flex-row md:justify-between">
+      <div
+        class="flex w-full flex-col items-center gap-4 md:flex-row md:justify-between xl:h-full xl:flex-col xl:items-end xl:gap-[3.25rem]"
+      >
         <ul class="flex items-center gap-4">
           <li>
             <a href="/">
