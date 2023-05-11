66f11f99fd826a52cf39633e54e5bcd049b6306c
Stephen Joseph Muñez <sjtmunez@gmail.com>
2023-05-11 12:25:29 +0800

    feat: cta desktop

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -4,16 +4,18 @@ import BgPatternHome6About5 from './icons/BgPatternHome6About5.vue';
 
 <template>
   <section
-    class="relative flex w-full flex-col items-center overflow-y-hidden bg-light-coral py-[5.188rem] md:py-[4.75rem]"
+    class="xl:py-[4.875re ] relative flex w-full flex-col items-center overflow-y-hidden bg-light-coral py-[5.188rem] md:py-[4.75rem]"
   >
-    <div class="pointer-events-none absolute bottom-0 left-0 translate-y-[22px]">
+    <div
+      class="pointer-events-none absolute bottom-0 left-0 translate-y-[22px] xl:translate-y-[0px]"
+    >
       <BgPatternHome6About5 />
     </div>
     <div
-      class="z-10 flex w-[87.2%] flex-col items-center gap-6 md:w-[74.60%] md:flex-row md:justify-between"
+      class="z-10 flex w-[87.2%] flex-col items-center gap-6 md:w-[74.60%] md:flex-row md:justify-between xl:w-[63.68%]"
     >
       <h2
-        class="text-center text-[2rem] font-bold leading-8 tracking-normal text-sacramento-state-green"
+        class="text-center text-[2rem] font-bold leading-8 tracking-normal text-sacramento-state-green xl:text-[3rem] xl:leading-[3rem]"
       >
         Ready to get started?
       </h2>
