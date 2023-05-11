2b40f440f30d8d89184aeb5cb6e36fa4948231f7
Stephen Joseph Muñez <sjtmunez@gmail.com>
2023-05-11 12:05:44 +0800

    feat: build and manage desktop

--- src/components/HomeBuildAndManage.vue
+++ src/components/HomeBuildAndManage.vue
@@ -8,19 +8,25 @@ import BgPatternHome3 from './icons/BgPatternHome3.vue';
 
 <template>
   <section
-    class="relative flex w-full flex-col items-center overflow-x-hidden bg-sacramento-state-green py-16 md:py-[6.25rem]"
+    class="relative flex w-full flex-col items-center overflow-x-hidden bg-sacramento-state-green py-16 md:py-[6.25rem] xl:py-[7.813rem]"
   >
-    <div class="pointer-events-none absolute top-0 right-0 translate-x-1/2">
+    <div
+      class="pointer-events-none absolute top-0 right-0 translate-x-1/2 xl:top-full xl:-translate-y-full"
+    >
       <BgPatternHome3 />
     </div>
-    <div class="z-10 flex w-[87.2%] flex-col items-center gap-14 md:w-[74.60%] md:gap-16">
-      <div class="flex w-full flex-col gap-8">
+    <div
+      class="z-10 flex w-[87.2%] flex-col items-center gap-14 md:w-[74.60%] md:gap-16 xl:w-[77.08%] xl:flex-row xl:items-start"
+    >
+      <div class="flex w-full flex-col gap-8 xl:w-[40.09%] xl:gap-14">
         <div class="h-1 w-[3.125rem] bg-light-coral"></div>
-        <h2 class="w-[73.39%] text-[2rem] font-bold leading-8 tracking-normal md:w-[77.66%]">
+        <h2
+          class="w-[73.39%] text-[2rem] font-bold leading-8 tracking-normal md:w-[77.66%] xl:w-full xl:text-[3rem] xl:leading-[3rem]"
+        >
           Build & manage distributed teams like no one else.
         </h2>
       </div>
-      <div class="flex w-full flex-col items-center gap-12 md:gap-8">
+      <div class="flex w-full flex-col items-center gap-12 md:gap-8 xl:mt-[3.625rem] xl:w-[48.64%]">
         <div
           class="flex flex-col items-center gap-4 md:flex-row md:items-start md:gap-6"
           v-for="feature in features"
